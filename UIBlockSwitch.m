adc50f61794bd7fda2ce33895ad3693d67e3113e
Josh <admin@unknown00250049f4a4.gateway.2wire.net>
2012-03-07 10:23:15 -0500

    Initial push'

---
+++ UIBlockSwitch.m
@@ -0,0 +1,38 @@
+//
+//  UIBlockSwitch.m
+//
+//  Created by Josh Pressnell on 3/7/12.
+//  Copyright (c) 2012 Verietas Software. All rights reserved.
+//
+
+#import "UIBlockSwitch.h"
+
+@interface UIBlockSwitch()
+{
+    SwitchBlock changeBlock;
+}
+
+@property (nonatomic,copy) SwitchBlock changeBlock;
+
+@end
+
+@implementation UIBlockSwitch
+
+@synthesize changeBlock;
+
++ (UIBlockSwitch*)switchWithState:(Boolean)initialValue onChange:(SwitchBlock)flipped
+{
+    UIBlockSwitch* theSwitch = [[[UIBlockSwitch alloc]initWithFrame:CGRectZero]autorelease];
+    [theSwitch addTarget:theSwitch action:@selector(switchWasFlipped) forControlEvents:UIControlEventValueChanged];
+    theSwitch.on = initialValue;
+    theSwitch.changeBlock = flipped;
+    return theSwitch;
+}
+
+- (void)switchWasFlipped
+{
+    if( changeBlock != nil )
+        changeBlock(self.on);
+}
+
+@end
